cb9fdc9ff047f41419c4f761bf390ba3adcf6226
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-09-04 17:28:05 +0300

    консолит состояние темы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './sass/main.scss';
+const toggle = document.querySelector('#theme-switch-toggle')
+const body = document.querySelector('body')
+toggle.addEventListener("change", addClassOperation)
+function addClassOperation(event) {
+   body.classList.toggle('dark-theme');
+   if(body.classList.value === 'dark-theme'){
+       localStorage.setItem("theme", "dark-theme")
+   console.log("темная тема")}
+   else (console.log("светлая тема")
+   )
+}
+const storageValue = localStorage.getItem("theme")
+console.log(storageValue)
+// function setTheme (){
+
+// }
