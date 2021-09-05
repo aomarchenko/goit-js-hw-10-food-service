1eefaaa69954cf9aa2a645affde56905d1c8ef6f
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-09-05 14:23:34 +0300

    почти готово

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-window.addEventListener("load", onLoad);
+import menuTemplete from './templates/menu-template.hbs';
+import menu from './menu.json';
+
+window.addEventListener('load', onLoad);
 function onLoad(load) {
-      let storageValue = localStorage.getItem("theme")
-if(body.classList.value!==localStorage.getItem("theme"))
-body.classList.add(storageValue)
-
-
-
+  let storageValue = localStorage.getItem('theme');
+  if (body.classList.value !== localStorage.getItem('theme')) body.classList.add(storageValue);
 }
-const switcher = document.querySelector(".theme-switch__toggle")
-const toggle = document.querySelector('#theme-switch-toggle')
-const body = document.querySelector('body')
-toggle.addEventListener("change", addClassOperation)
+const switcher = document.querySelector('.theme-switch__toggle');
+const toggle = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+const markupContainer = document.querySelector('.js-menu');
+const cardsMarkup = createMenuMarkup(menu);
+
+toggle.addEventListener('change', addClassOperation);
 function addClassOperation(event) {
-   body.classList.toggle('dark-theme');
-   if(body.classList.value==='dark-theme'){
-       
-        
+  body.classList.toggle('dark-theme');
+  //    if(body.classList.value==='dark-theme'){
 
-   }
-   
-
+  //    }
 }
 
-
-toggle.addEventListener("change", changeStorageValue)
-function changeStorageValue(){
-    localStorage.setItem("theme", body.classList.value) 
-    
+toggle.addEventListener('change', changeStorageValue);
+function changeStorageValue() {
+  localStorage.setItem('theme', body.classList.value);
 }
 
+window.addEventListener('load', isToggleChecked);
 
-window.addEventListener("load", isToggleChecked)
-
-function isToggleChecked(){
-    if(body.classList.value==='dark-theme')
-    document.querySelector(".theme-switch__toggle").checked = true
+function isToggleChecked() {
+  if (body.classList.value === 'dark-theme')
+    document.querySelector('.theme-switch__toggle').checked = true;
 }
-console.log(document.querySelector(".theme-switch__toggle").checked)
+
+console.log(menu);
+
+function createMenuMarkup(menu) {
+  return menu.map(menuTemplete).join('');
+}
+console.log(cardsMarkup);
+console.log(markupContainer);
+markupContainer.insertAdjacentHTML('beforeend', cardsMarkup);
